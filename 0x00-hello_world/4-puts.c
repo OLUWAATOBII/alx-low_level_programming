278eb0fd158a28bf05c276895975181be6a80589
oluwaatobii <onafuwaoluwatobi@gmail.com>
2023-02-20 13:54:07 -0800

    my first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: program that print multilingual puzzle
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
